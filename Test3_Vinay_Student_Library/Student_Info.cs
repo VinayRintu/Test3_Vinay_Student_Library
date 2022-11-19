4037d58a4bd8aa566140b5e6e5d49cee90f7aba2
Rintu <Rintu@VINDUJA>
2022-11-19 15:58:47 +0530

    Add project files.

---
+++ Test3_Vinay_Student_Library/Student_Info.cs
@@ -0,0 +1,28 @@
+﻿namespace Test3_Vinay_Student_Library
+{
+    public class Student_Info
+    {
+        public int StudentID { get; set; }
+        public string? StudentName { get; set; }
+        public int Standard { get; set; }
+
+        public void SetStudentDetails()
+        {
+            Console.WriteLine("Enter Student ID");
+            StudentID=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Student Nme");
+            StudentName = Console.ReadLine();
+
+            Console.WriteLine("Enter Student Standard");
+            Standard = int.Parse(Console.ReadLine());
+        }
+
+        public void DisplayStudentDetails()
+        {
+            Console.WriteLine("Student ID : " + StudentID);
+            Console.WriteLine("Student Nme : "+ StudentName);
+            Console.WriteLine("Student Standard : "+ Standard);
+        }
+    }
+}
